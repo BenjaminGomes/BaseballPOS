145f042bf4dabc99f5cfa1abe8ae85cd632640c3
BenjaminGomes <BenjaminGomes@users.noreply.github.com>
2015-08-11 19:15:29 -0500

    Testing the comment boxes

--- SportingGoods/SportingGoods/baseball.cpp
+++ SportingGoods/SportingGoods/baseball.cpp
@@ -1,35 +1,35 @@
 /*
 *********************************************************************************************
-*																							*
-*		Programmer:					Ben Gomes												*
-*		Instructor:					David Wilke												*
-*		Programming Assignment:		Jonny Mack's Sporting Goods								*
-*		Program .cpp:				Baseball.cpp											*
-*		Date:						15 November 2013										*
-*																							*
-*		This program will propmt the user with a menu which they can select one of six		*
-*		sale items. The program will error check and make sure the user entered in a		*
-*		corect number for the menu options (Ranging from 1-6), if not it prompts the user	*
-*		to re-enter in a valid option. After the user has selected which product they		*
-*		would like, the user is prompted for the quantity they would like to purchase of	*
-*		that specific item. The program then alculates the cost for the product(s) and		*
-*		displays the total. The user is then prompted if they would like to purchase		*
-*		another item, if 'Yes' the user is taken back to the main menu where they can add	*
-*		other purchases. If the user chooses 'No', they are created a receipt (in the		*
-*		form of a *.txt file) that is written by the program. THe receipt displays the		*
-*		total quantities the user is wanted of each item and the total costs per each		*
-*		sales item. THere is also an error check for the input asking the user if they		*
-*		would like to return to the main menu.												*
-*																							*
+*																							
+*		Programmer:					Ben Gomes												
+*		Instructor:					David Wilke												
+*		Programming Assignment:		Jonny Mack's Sporting Goods								
+*		Program .cpp:				Baseball.cpp											
+*		Date:						15 November 2013										
+*																							
+*		This program will propmt the user with a menu which they can select one of six		
+*		sale items. The program will error check and make sure the user entered in a		
+*		corect number for the menu options (Ranging from 1-6), if not it prompts the user	
+*		to re-enter in a valid option. After the user has selected which product they		
+*		would like, the user is prompted for the quantity they would like to purchase of	
+*		that specific item. The program then alculates the cost for the product(s) and		
+*		displays the total. The user is then prompted if they would like to purchase		
+*		another item, if 'Yes' the user is taken back to the main menu where they can add	
+*		other purchases. If the user chooses 'No', they are created a receipt (in the		
+*		form of a *.txt file) that is written by the program. THe receipt displays the		
+*		total quantities the user is wanted of each item and the total costs per each		
+*		sales item. THere is also an error check for the input asking the user if they		
+*		would like to return to the main menu.												
+*																							
 *********************************************************************************************
 
 */
 
 /*
 *********************************************
-*											*
-*			Prepocessor Directives			*
-*											*
+*											
+*		Prepocessor Directives			
+*											
 *********************************************
 */
 
@@ -43,9 +43,9 @@
 
 /*
 *********************************************
-*											*
-*				Namespace					*
-*											*
+*											
+*		Namespace					
+*											
 *********************************************
 */
 
@@ -53,9 +53,9 @@ using namespace std;
 
 /*
 *********************************************
-*											*
-*			Function Prototypes				*
-*											*
+*											
+*		Function Prototypes				
+*											
 *********************************************
 */
 
@@ -68,9 +68,9 @@ void ExitScreen();
 
 /*
 *********************************************
-*											*
-*				Constant Variable			*
-*											*
+*											
+*		Constant Variable		
+*											
 *********************************************
 */
 
@@ -78,10 +78,10 @@ const int SIZE = 6;
 
 /*
 *********************************************
-*											*
-*		Creating Object Reference To		*
-*			BaseBallItems Header			*
-*											*
+*											
+*		Creating Object Reference To		
+*		BaseBallItems Header			
+*											
 *********************************************
 */
 
@@ -89,9 +89,9 @@ BaseBallItems Items;
 
 /*
 *********************************************************************************************
-*																							*
-*											Main Class										*
-*																							*
+*																							
+*											Main Class										
+*																							
 *********************************************************************************************
 */
 
